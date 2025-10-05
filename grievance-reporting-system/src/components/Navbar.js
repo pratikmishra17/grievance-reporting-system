0de4216800dd5b546bda736a5408cf4061071277
Pratik Mishra <pratikmishra2807@gmail.com>
2025-10-05 14:10:57 -0400

    first commit

---
+++ grievance-reporting-system/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div>
+      <h2>Hello from MyComponent!</h2>
+      <p>This is a reusable component.</p>
+    </div>
+  );
+}
+
+export default Navbar;
